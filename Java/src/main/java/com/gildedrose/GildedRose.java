75615f2a7fe85f106545170a3cde33f9b7b9a5fe
Paolo Sciarra <itbhps@gmail.com>
2025-02-01 10:27:29 +0100

    inverted if conditions

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 class GildedRose {
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -16,51 +19,50 @@ class GildedRose {
      */
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
+            if (items[i].name.equals(AGED_BRIE) || items[i].name.equals(BACKSTAGE)) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                    if (items[i].name.equals(BACKSTAGE)) {
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        if (items[i].sellIn < 6 && items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
+                    }
+                }
+            } else {
+                if (items[i].quality > 0) {
+                    if (items[i].name.equals(SULFURAS)) {
+                        continue;
+                    } else {
+                        items[i].quality = items[i].quality - 1;
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (items[i].name.equals(SULFURAS)) {
+                continue;
+            } else {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
+                if (items[i].name.equals(AGED_BRIE)) {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
+                    }
+                } else {
+                    if (items[i].name.equals(BACKSTAGE)) {
+                        items[i].quality = items[i].quality - items[i].quality;
+                    } else {
+                        if (items[i].quality > 0) {
+                            if (items[i].name.equals(SULFURAS)) {
+                                continue;
+                            }
+                            items[i].quality = items[i].quality - 1;
+                        }
                     }
                 }
             }
