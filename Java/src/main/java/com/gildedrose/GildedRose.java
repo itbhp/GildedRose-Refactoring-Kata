9d6fc968898c9f59cdc1c687fa78e94e8d020895
Paolo Sciarra <itbhps@gmail.com>
2025-02-08 09:44:27 +0100

    moved logic so similar branches, removed duplicated similar branches and introduced a switch condition

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -19,53 +19,47 @@ class GildedRose {
      */
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals(AGED_BRIE)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-                item.sellIn = item.sellIn - 1;
-            } else if (item.name.equals(BACKSTAGE)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.sellIn < 11 && item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                    if (item.sellIn < 6 && item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-                item.sellIn = item.sellIn - 1;
-            } else if (item.name.equals(SULFURAS)) {
-                // do nothing
-            } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-                item.sellIn = item.sellIn - 1;
-            }
-
-
-            if (item.name.equals(AGED_BRIE)) {
-                if (item.sellIn < 0) {
+            switch (item.name) {
+                case AGED_BRIE:
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
-                }
-            } else if (item.name.equals(BACKSTAGE)) {
-                if (item.sellIn < 0) {
-                    item.quality = item.quality - item.quality;
-                }
-            } else {
-                if (item.sellIn < 0) {
+                    item.sellIn = item.sellIn - 1;
+                    if (item.sellIn < 0 && item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                    break;
+                case BACKSTAGE:
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+
+                        if (item.sellIn < 11 && item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                        if (item.sellIn < 6 && item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                    item.sellIn = item.sellIn - 1;
+                    if (item.sellIn < 0) {
+                        item.quality = item.quality - item.quality;
+                    }
+                    break;
+                case SULFURAS:
+                    // do nothing
+                    break;
+                default:
                     if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                    item.sellIn = item.sellIn - 1;
+                    if (item.sellIn < 0 && item.quality > 0) {
                         if (!item.name.equals(SULFURAS)) {
                             item.quality = item.quality - 1;
                         }
                     }
-                }
+                    break;
             }
-
         }
     }
 }
