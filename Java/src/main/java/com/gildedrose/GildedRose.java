35882a6ea6cfce05908272c3fd52efdf8d293595
Paolo Sciarra <itbhps@gmail.com>
2025-02-08 09:47:30 +0100

    cleaned up switch case

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -32,14 +32,14 @@ class GildedRose {
                 case BACKSTAGE:
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
-
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
                     }
+                    if (item.sellIn < 11 && item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                    if (item.sellIn < 6 && item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+
                     item.sellIn = item.sellIn - 1;
                     if (item.sellIn < 0) {
                         item.quality = item.quality - item.quality;
@@ -54,9 +54,7 @@ class GildedRose {
                     }
                     item.sellIn = item.sellIn - 1;
                     if (item.sellIn < 0 && item.quality > 0) {
-                        if (!item.name.equals(SULFURAS)) {
-                            item.quality = item.quality - 1;
-                        }
+                        item.quality = item.quality - 1;
                     }
                     break;
             }
